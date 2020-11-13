72aa5314e2735c2db8b4c9894e2f696b4ac6397a
Projaro <services@projaro.com>
2020-11-13 20:05:58 +0100

    Search result update

--- assets/js/price-slider.js
+++ assets/js/price-slider.js
@@ -2,13 +2,13 @@
     $( "#slider-range" ).slider({
       range: true,
       min: 0,
-      max: 5000,
-      values: [ 0, 2000 ],
+      max: 200000,
+      values: [ 0, 200000 ],
       slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+        $( "#amount" ).val( "" + ui.values[ 0 ] + " - " + ui.values[ 1 ] );
       }
     });
-    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+    $( "#amount" ).val( "" + $( "#slider-range" ).slider( "values", 0 ) +
+      " - " + $( "#slider-range" ).slider( "values", 1 ) );
   } );
  
